8409067bbc3d60f93e75f2deae1fd9a402505942
wangyangzhi <backstreetwx@gmail.com>
2016-12-06 19:44:28 +0900

    commit

---
+++ Fuguoji/Assets/Scripts/Attack.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+
+
+  public Text PlayerHPText;
+  public Text EnemeyHPText;
+  public int AttackMin;
+  public int AttackMax;
+
+
+  private int defaultPlayerHP;
+  private int defaultEnemeyHP;
+
+	// Use this for initialization
+	void Start () {
+    
+    defaultPlayerHP = 100;
+    defaultEnemeyHP = 100;
+    PlayerHPText.text = defaultPlayerHP.ToString ();
+    EnemeyHPText.text = defaultEnemeyHP.ToString ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+    PlayerHPText.text = defaultPlayerHP.ToString ();
+    EnemeyHPText.text = defaultEnemeyHP.ToString ();
+	}
+
+  public void AttackAction(){
+    int _attackOfPlayer = Random.Range (AttackMin, AttackMax);
+    int _attackOfEnemey = Random.Range (AttackMin, AttackMax);
+    Debug.Log ("Player attack : " + _attackOfPlayer.ToString());
+    Debug.Log ("Enemey attack : " + _attackOfEnemey.ToString());
+
+    defaultPlayerHP = defaultPlayerHP - _attackOfEnemey;
+    defaultEnemeyHP = defaultEnemeyHP - _attackOfPlayer;
+  } 
+}
---
+++ Fuguoji/Assets/Scripts/GameOver.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+  public Text PlayerHPText;
+  public Text EnemyHPText;
+  public Text GameOverText;
+  public Text GameRestartText;
+
+
+  private string playerHP;
+  private string enemyHP;
+
+  private bool gameOver;
+  private bool restart;
+
+	// Use this for initialization
+	void Start () {
+    playerHP = PlayerHPText.text;
+    enemyHP  = EnemyHPText.text;
+    gameOver = false;
+    restart = false;
+	}
+	
+	// Update is called once per frame
+  void FixedUpdate () {
+    playerHP = PlayerHPText.text;
+    enemyHP  = EnemyHPText.text;
+
+    if (int.Parse(playerHP) <= 0&& int.Parse(enemyHP)>0) {
+      GameOverText.text = "Player Lose";
+      gameOver = true;
+    }
+
+    if (int.Parse(playerHP) > 0&& int.Parse(enemyHP)<=0) {
+      GameOverText.text = "Player Win";
+      gameOver = true;
+    }
+
+    if (int.Parse(playerHP) <= 0&& int.Parse(enemyHP)<=0) {
+      GameOverText.text = "Both Dead";
+      gameOver = true;
+    }
+
+    if (gameOver) {
+      GameRestartText.text = "Press 'R' for Restart";
+      restart = true;
+    }
+
+    if (restart) {
+      
+      if (Input.GetKeyDown (KeyCode.R))
+      {
+        Application.LoadLevel (Application.loadedLevel);
+      }
+    }
+
+	}
+}
